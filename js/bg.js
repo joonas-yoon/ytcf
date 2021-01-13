b03f13e4a22de43b8159b8ee2654509a6d8a3d48
Joona Yoon <joonas.yoon@gmail.com>
2021-01-13 19:03:40 +0900

    Optimize with MutationObserver

--- js/bg.js
+++ js/bg.js
@@ -1,27 +1,56 @@
+'use strict';
+
 function hide(e, result) {
   // if (!result.isReliable) return;
   const lang = result.languages[0];
   // TODO: set language from option
   if (lang.language !== 'ko') {
-    e.classList.add('hidden');
-    e.parentNode.classList.add('hidden');
+    (e.parentNode || e).remove();
   } else {
     e.classList.add('filtered');
   }
 }
 
-function hideComments(){
-  const e = document.querySelectorAll('#comment.ytd-comment-thread-renderer:not(.filtered)');
+function hideComments(comments, checked){
+  let e = comments.querySelectorAll('.ytd-comment-thread-renderer:not(.filtered)');
   for(let i = 0; i < e.length; ++i){
     const _e = e[i];
-    chrome.i18n.detectLanguage(
-      _e.querySelector('#content-text.ytd-comment-renderer').innerText,
-      (result) => { hide(_e, result); }
-    );
+    const txt = _e.querySelector('yt-formatted-string#content-text');
+    if (txt) {
+      chrome.i18n.detectLanguage(
+        txt.innerText,
+        (result) => { hide(_e, result); }
+      );
+    }
   }
-  setTimeout(hideComments, 1000);
+  // double check
+  if (!checked) {
+    setTimeout(hideComments.bind(null, comments, true), 500);
+  }
+}
+
+function setup(){
+  var target = document.querySelector('ytd-item-section-renderer#sections');
+  if (target == null) {
+    setTimeout(setup, 1000);
+    return;
+  }
+
+  var observer = new MutationObserver(function(mutations) { 
+    mutations.forEach(function(mutation) { 
+      setTimeout(hideComments.bind(null, mutation.target), 10);
+    }); 
+  });
+
+  var config = { 
+    attributes: true,
+    childList: true,
+    characterData: true
+  }; 
+
+  observer.observe(target, config);
 }
 
 (function(){
-  hideComments();
+  setup();
 })();
