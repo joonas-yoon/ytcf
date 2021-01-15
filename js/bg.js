4e7edab3cc1e79c8fb30263d88e7814a2a39eebd
Joona Yoon <joonas.yoon@gmail.com>
2021-01-15 17:31:38 +0900

    feat: button to enable filter

--- js/bg.js
+++ js/bg.js
@@ -1,14 +1,16 @@
 'use strict';
 
+// TODO: hide and show by css
+// TODO: just add language into class list
+// TODO: change input/select, show/hide css
+
 function hide(e, result) {
-  // if (!result.isReliable) return;
-  const lang = result.languages[0];
-  // TODO: set language from option
-  if (lang.language !== 'ko') {
-    (e.parentNode || e).remove();
-  } else {
-    e.classList.add('filtered');
+  let langs = result && result.languages.length ? ' ' : ' und';
+  for (let i = 0; i < result.languages.length; ++i) {
+    langs += ' ' + result.languages[i].language;
   }
+  let reliableClass = 'reliable';
+  e.setAttribute('class', e.getAttribute('class') + ' filtered ' + langs + ' ' + (result.isReliable ? reliableClass : ''));
 }
 
 function hideComments(comments, checked){
@@ -30,16 +32,16 @@ function hideComments(comments, checked){
 }
 
 function setup(){
-  var target = document.querySelector('ytd-item-section-renderer#sections');
-  if (target == null) {
+  let target = document.querySelector('ytd-item-section-renderer#sections');
+  if (!target) {
     setTimeout(setup, 1000);
     return;
   }
 
-  var observer = new MutationObserver(function(mutations) { 
+  var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) { 
-      setTimeout(hideComments.bind(null, mutation.target), 10);
-    }); 
+      setTimeout(hideComments.bind(null, mutation.target, false), 10);
+    });
   });
 
   var config = { 
@@ -49,6 +51,44 @@ function setup(){
   }; 
 
   observer.observe(target, config);
+
+  injectUI(target);
+}
+
+function injectUI(container){
+  var path = chrome.extension.getURL('css/style.css');
+  var css = document.createElement('link');
+  css.setAttribute('rel', 'stylesheet');
+  css.setAttribute('type', 'text/css');
+  css.setAttribute('href', path);
+  document.getElementsByTagName('head')[0].appendChild(css);
+
+  let node = document.createElement('div');
+  node.setAttribute('class', 'ytcf-wrapper');
+  let swt = document.createElement('div');
+  swt.setAttribute('class', 'ytcf-switch');
+  let inp = document.createElement('input');
+  inp.setAttribute('id', 'ytcf-switch');
+  inp.setAttribute('type', 'checkbox');
+  inp.setAttribute('class', 'ytcf-switch-input');
+  let lab = document.createElement('label');
+  lab.setAttribute('for', inp.id);
+  lab.setAttribute('class', 'ytcf-switch-label');
+  swt.appendChild(inp);
+  swt.appendChild(lab);
+  node.appendChild(swt);
+  let text = document.createElement('div');
+  text.setAttribute('class', 'ytcf-label');
+  text.innerText = 'Enable Language Filter';
+  node.appendChild(text);
+  container.insertBefore(node, container.firstChild);
+  inp.addEventListener('change', function(e){
+    if (e.target.checked) {
+      container.classList.add('ytcf-enabled');
+    } else {
+      container.classList.remove('ytcf-enabled');
+    }
+  }, false);
 }
 
 (function(){
